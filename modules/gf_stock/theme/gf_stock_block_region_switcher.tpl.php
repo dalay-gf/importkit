b33e24d04649f7bffdd24ea4ee461fcd01a8e581
dalay <dalay@drupalka.ru>
2019-03-10 12:22:00 +0300

    mv gf_stock to parent location

--- modules/gf_stock/theme/gf_stock_block_region_switcher.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php if ($regions): ?>
-<ul>
-<?php foreach($regions as $region_name => $region_link): ?>
-  <li<?php if ($current_region == $region_name) 
-              print ' class="active"'; ?>><?php print $region_link; ?></li>
-<?php endforeach; ?>
-</ul>
-<?php endif; ?>
